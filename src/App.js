085014cc1e8180d167582b2e7c8447c28e299dc6
akhiang <akhiang32@gmail.com>
2021-05-23 22:28:14 +0700

    split more component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import Todo from './components/Todo'
+import { Route, Switch } from 'react-router-dom'
+
+import AllMeetupsPage from './pages/AllMeetups'
+import NewMeetupPage from './pages/NewMeetup'
+import FavoritesPage from './pages/Favorites'
+import MainNavigation from './layout/MainNavigation'
 
 function App() {
   return (
     <div>
-      <h1>My Todos</h1>
-      <Todo text="Judul 1" />
-      <Todo text="Judul 2" />
-      <Todo text="Judul 3" />
+      <MainNavigation />
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetupPage />
+        </Route>
+        <Route path="/favorites">
+          <FavoritesPage />
+        </Route>
+      </Switch>
     </div>
   )
 }
